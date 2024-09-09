fb76718be4fa7c87855e5e49a20482d4a52841ad
diogo <diogopelaes@gmail.com>
2024-09-09 11:09:19 -0300

    Aula02

--- gerenciador/views.py
+++ gerenciador/views.py
@@ -1,3 +1,18 @@
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render
 
-# Create your views here.
+
+# view que renderiza index.html
+def index(request):
+    if request.method == 'POST':
+        username = request.POST['username'].strip()
+        password = request.POST['password'].strip()
+        user = authenticate(request, username=username, password=password)
+        if user is None:
+            context = {
+                'alerta': 'Usuário ou senha incorreta'
+            }
+            return render(request, 'index.html', context)
+        else:
+            login(request, user)
+    return render(request, 'index.html')
